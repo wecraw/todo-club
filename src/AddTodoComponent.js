15defc3ac26368547b469c58e49a0290f1e34ae2
Will Crawford <will@ccrawford.org>
2022-03-06 13:05:18 -0800

    working component

--- src/AddTodoComponent.js
+++ src/AddTodoComponent.js
@@ -1,17 +1,88 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+import { API, Storage } from 'aws-amplify';
 
-export default class LoginComponent extends Component {
+const initialFormState = { name: '', description: '' }
 
-constructor(props) {
-    super(props)
-    }
 
-render() {
-    const element = (<div>Text from Element</div>)
-    return (<div className="comptext">
-    <h3>First Component</h3>
-        {this.props.displaytext}
-        {element}
-    </div>)
-    }
-}
+function AddTodo(props) {
+        const [todos, setTodos] = useState([]);
+        const [formData, setFormData] = useState(initialFormState);
+        
+        useEffect(() => {
+          fetchTodos();
+        }, []);
+        
+        async function fetchTodos() {
+          const apiData = await API.graphql({ query: listTodos });
+          const todosFromAPI = apiData.data.listTodos.items;
+          await Promise.all(todosFromAPI.map(async todo => {
+            if (todo.image) {
+              const image = await Storage.get(todo.image);
+              todo.image = image;
+            }
+            return todo;
+          }))
+          setTodos(apiData.data.listTodos.items);
+        }
+        
+        async function onChange(e) {
+          if (!e.target.files[0]) return
+          const file = e.target.files[0];
+          setFormData({ ...formData, image: file.name });
+          await Storage.put(file.name, file);
+          fetchTodos();
+        }
+        
+        async function createTodo() {
+          if (!formData.name || !formData.description) return;
+          await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+          if (formData.image) {
+            const image = await Storage.get(formData.image);
+            formData.image = image;
+          }
+          setTodos([ ...todos, formData ]);
+          setFormData(initialFormState);
+        }
+        
+        async function deleteTodo({ id }) {
+          const newTodosArray = todos.filter(todo => todo.id !== id);
+          setTodos(newTodosArray);
+          await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+        }
+    
+        return (
+            <div>
+
+                <input
+                onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+                placeholder="Todo name"
+                value={formData.name}
+            />
+            <input
+                onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+                placeholder="Todo description"
+                value={formData.description}
+            />
+            <button onClick={createTodo}>Create Todo</button>
+            <div style={{marginBottom: 30}}>
+                {
+                todos.map(todo => (
+                    <div key={todo.id || todo.name}>
+                    <h2>{todo.name}</h2>
+                    <p>{todo.description}</p>
+                    <button onClick={() => deleteTodo(todo)}>Delete todo</button>
+                    {
+                        todo.image && <img src={todo.image} style={{width: 400}} />
+                    }
+                    </div>
+                ))
+                }
+            </div> 
+          </div>
+
+        )
+} 
+
+export default AddTodo;
